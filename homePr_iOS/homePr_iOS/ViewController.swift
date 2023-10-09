e1ef6d29d60a43025afbd74024487dbc217a4c7e
danya <d4k07@yandex.ru>
2023-10-09 19:02:07 +0300

    xcodeapp created

---
+++ homePr_iOS/homePr_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homePr_iOS
+//
+//  Created by Данил Клюев on 09.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
